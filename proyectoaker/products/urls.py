b9338201a3fb2c8e47e2dab74f67aef047e35558
Senk0 <francoorlando11@gmail.com>
2024-05-03 20:02:34 +0200

    Products Fix
    Se arregla products.html

--- proyectoaker/products/urls.py
+++ proyectoaker/products/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("products/", views.productsListView.as_view(), name="products"),
     path("search-products/", views.productsListByKword.as_view(), name="search-products"),
     path("detail/<pk>/", views.productDetail.as_view(), name="detail"),
-
+    path("404-error/", views.errorView.as_view(), name="404-error"),
 ]
 
--- proyectoaker/proyectoaker/settings.py
+++ proyectoaker/proyectoaker/settings.py
@@ -1,21 +1,23 @@
 import os
 from pathlib import Path
+#from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+#load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tn6$ya1d4$%fay&nm-$(zze%m$*8*nsud0w1h=ait*4waiy6)t'
+SECRET_KEY = 'EmQwvGbJPHWJVmK4XC1viOsidmbRo1YL4ChDs9TYrvxeWvfRDI'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 APP_NAME = os.environ.get("FLY_APP_NAME")
-#ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
-#CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
+ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
+CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
 
 # Application definition
 
@@ -104,7 +106,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
